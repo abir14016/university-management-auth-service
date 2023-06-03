5c625e09339fa48ac84812aafe80f6ddf7873c7f
abir14016 <abir14016@gmail.com>
2023-06-03 15:30:37 +0600

    setup eslint, pretties, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
-const port = 3000;
 
 app.use(cors());
 
@@ -10,8 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
 
 async function bootstrap() {
   try {
