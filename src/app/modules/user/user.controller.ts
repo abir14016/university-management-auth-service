80fe5678359a9e61e7a3cb5d8e42a307776341f8
abir14016 <abir14016@gmail.com>
2023-06-09 08:27:19 +0600

    [DONE] optimized try catch block and api response

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,30 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
     const result = await UserService.createUser(user);
-    res.status(200).json({
+
+    next();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'user created successfully',
       data: result,
     });
-  } catch (error) {
-    next(error);
+
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'user created successfully',
+    //   data: result,
+    // });
   }
-};
+);
 
 export const UserController = {
   createUser,
