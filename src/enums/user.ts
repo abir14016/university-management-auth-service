fce4893477c357f066a0f3ec4cef8e3a2cc00e1c
abir14016 <abir14016@gmail.com>
2023-06-21 17:01:22 +0600

    [DONE] emum created for specific role

---
+++ src/enums/user.ts
@@ -0,0 +1,10 @@
+export enum ENUM_USER_ROLE {
+  // eslint-disable-next-line no-unused-vars
+  SUPER_ADMIN = 'super_admin',
+  // eslint-disable-next-line no-unused-vars
+  ADMIN = 'admin',
+  // eslint-disable-next-line no-unused-vars
+  STUDENT = 'student',
+  // eslint-disable-next-line no-unused-vars
+  FACULTY = 'faculty',
+}
