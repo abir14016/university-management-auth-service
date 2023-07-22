e3c613ee194592d0ab7cdb90dde4b83abcf80a89
abir14016 <abir14016@gmail.com>
2023-07-22 08:35:06 +0600

    [DONE] auth valiation in academic faculty route level

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -2,30 +2,59 @@ import express from 'express';
 import { AcademicFacultyController } from './academicFaculty.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyValidation } from './academicFaculty.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 //router for creating a academic faculty
 router.post(
   '/create-faculty',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(AcademicFacultyValidation.createFacultyZodSchema),
   AcademicFacultyController.createFaculty
 );
 
 //router for getting a single academic faculty by id
-router.get('/:id', AcademicFacultyController.getSingleFaculty);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicFacultyController.getSingleFaculty
+);
 
 //router for updating academic faculty
 router.patch(
   '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY
+  ),
   validateRequest(AcademicFacultyValidation.updateFacultyZodSchema),
   AcademicFacultyController.updateFaculty
 );
 
 //router for deleting academic faculty
-router.delete('/:id', AcademicFacultyController.deleteFaculty);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicFacultyController.deleteFaculty
+);
 
 //router for getting all academic faculties
-router.get('/', AcademicFacultyController.getAllFaculties);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicFacultyController.getAllFaculties
+);
 
 export const AcademicFacultyRoutes = router;
