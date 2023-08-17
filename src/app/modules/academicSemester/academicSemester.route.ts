882372bd52b9e8a821bc36eb3a047fd136582c8f
abir14016 <abir14016@gmail.com>
2023-08-17 21:18:51 +0600

    [DONE] finish role-based authentication for all routes

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterValidation } from './academicsemester.validation';
 import { AcademicSemesterController } from './acdemicSemester.controller';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
@@ -9,24 +11,48 @@ const router = express.Router();
 router.post(
   '/create-semester',
   validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.createSemester
 );
 
 //router for getting single semsester
-router.get('/:id', AcademicSemesterController.getSingleSemester);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicSemesterController.getSingleSemester
+);
 
 //router for update semester
 // **patch[update] must be always before get all documents route**
 router.patch(
   '/:id',
   validateRequest(AcademicSemesterValidation.updateAcademicSemesterZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.updateSemester
 );
 
 //router for deleting single semester
-router.delete('/:id', AcademicSemesterController.deleteSemester);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteSemester
+);
 
 //router for getting all semsesters
-router.get('/', AcademicSemesterController.getAllSemesters);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicSemesterController.getAllSemesters
+);
 
 export const AcademicSemesterRoutes = router;
